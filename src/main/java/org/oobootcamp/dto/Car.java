7af6ed327a680b3e5a5e2cce9f7291ab5fcd977d
zhuang.mei <zhuang.mei@cat.com>
2022-08-22 15:27:39 +0800

    fix: add field for dto class.

--- src/main/java/org/oobootcamp/dto/Car.java
+++ src/main/java/org/oobootcamp/dto/Car.java
@@ -1,6 +1,10 @@
 package org.oobootcamp.dto;
 
 public class Car {
+
+  private String licensePlateNumber;
+
   public Car(String licensePlateNumber) {
+    this.licensePlateNumber = licensePlateNumber;
   }
 }
--- src/main/java/org/oobootcamp/dto/Ticket.java
+++ src/main/java/org/oobootcamp/dto/Ticket.java
@@ -1,5 +1,13 @@
 package org.oobootcamp.dto;
 
+import java.util.UUID;
+
 public class Ticket {
+
+  private String ticketNumber;
+
+  public Ticket() {
+    ticketNumber = UUID.randomUUID().toString();
+  }
 
 }
